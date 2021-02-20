fa0d4a765cf251e92bae0636d161fa063b6d7446
Dennis José da Silva <dennisjosesilva@gmail.com>
2021-02-20 10:20:13 -0300

    remove stb libs from dependencies.

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,11 @@
 using namespace morphotree;
 
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+// #define STB_IMAGE_IMPLEMENTATION
+// #include "stb_image.h"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+// #define STB_IMAGE_WRITE_IMPLEMENTATION
+// #include "stb_image_write.h"
 
 
 using tree_t = MorphologicalTree<uint8>;
