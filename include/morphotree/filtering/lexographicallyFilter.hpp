d95d5c5537bd08068f449da83924ba0fcfcb176e
Dennis José da Silva <dennisjosesilva@gmail.com>
2021-07-13 16:36:01 -0300

    lexographically filter implementation.

---
+++ include/morphotree/filtering/lexographicallyFilter.hpp
@@ -0,0 +1,37 @@
+#pragma once 
+
+#include "morphotree/core/alias.hpp"
+#include "morphotree/tree/mtree.hpp"
+#include <vector>
+
+namespace morphotree
+{
+  template<class ValueType>
+  MorphologicalTree<ValueType> filterTreeLexographical(const MorphologicalTree<ValueType> &tree, 
+    const std::vector<uint32> &order, uint32 numberOfNodesToBeKept);  
+  
+  template<class ValueType>
+  void ifilterTreeLexographical(MorphologicalTree<ValueType> &tree, 
+    const std::vector<uint32> &order, uint32 numberOfNodesToBeKept);
+
+  // ========================== [ IMPLEMENTATION ] ================================================
+  template<class ValueType>
+  void ifilterTreeLexographical(const MorphologicalTree<ValueType> &tree, 
+    const std::vector<uint32> &order, uint32 numberOfNodesToBeKept)
+  {
+    using NodePtr = typename MorphologicalTree<ValueType>::NodePtr;
+
+    tree.idirectFilter([&order, numberOfNodesToBeKept](NodePtr node) {
+      return order[node->id()] < numberOfNodesToBeKept;
+    });
+  }
+
+  template<class ValueType>
+  MorphologicalTree<ValueType> filterTreeLexographical(const MorphologicalTree<ValueType> &tree,
+    const std::vector<uint32> &order, uint32 numberOfNodesToBeKept)
+  {
+    MorphologicalTree<ValueType> ctree = tree.copy();
+    ifilterTreeLexographical(ctree, order, numberOfNodesToBeKept);
+    return ctree;
+  }
+}
