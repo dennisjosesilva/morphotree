492a15283708c07f2e424cf7f22bbda66384ec41
Dênnis José da Silva <dennisjosesilva@gmail.com>
2022-09-19 15:46:15 +0200

    adding support to load image from disc at the example application of the contour extraction.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 #include "morphotree/core/io.hpp"
 
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 namespace mt = morphotree;
 
 int main(int argc, char *argv[])
@@ -20,41 +26,73 @@ int main(int argc, char *argv[])
   using mt::extractCountors;
   using mt::reconstructContourImage;
   using mt::UI32Point;
+  using mt::I32Point;
   using mt::buildMaxTree;
   using mt::printImageIntoConsoleWithCast;
 
-  std::vector<uint8> f = {
-    0, 0, 0, 0, 0, 0, 0,
-    0, 4, 4, 4, 7, 7, 7,
-    0, 7, 7, 4, 7, 4, 7,
-    0, 7, 4, 4, 7, 4, 7,
-    0, 4, 4, 4, 7, 4, 7,
-    0, 7, 7, 4, 7, 7, 7,
-    0, 0, 0, 0, 0, 0, 0,
-  };
+  if (argc < 2) {
+    std::vector<uint8> f = {
+      0, 0, 0, 0, 0, 0, 0,
+      0, 4, 4, 4, 7, 7, 7,
+      0, 7, 7, 4, 7, 4, 7,
+      0, 7, 4, 4, 7, 4, 7,
+      0, 4, 4, 4, 7, 4, 7,
+      0, 7, 7, 4, 7, 7, 7,
+      0, 0, 0, 0, 0, 0, 0,
+    };
 
-  Box domain = Box::fromSize(UI32Point{7,7});
-  std::shared_ptr<Adjacency> adj 
-    = std::make_shared<Adjacency4C>(domain);
-  MTree tree = buildMaxTree(f, adj);
+    Box domain = Box::fromSize(UI32Point{7,7});
+    std::shared_ptr<Adjacency> adj 
+      = std::make_shared<Adjacency4C>(domain);
+    MTree tree = buildMaxTree(f, adj);
 
-  std::vector<std::set<uint32>> countors = 
-    extractCountors(
-      domain, 
-      f, 
-      std::make_shared<InfAdjacency4C>(domain), 
-      tree);
+    std::vector<std::set<uint32>> countors = 
+      extractCountors(
+        domain, 
+        f, 
+        std::make_shared<InfAdjacency4C>(domain), 
+        tree);
 
-  tree.traverseByLevel([&domain, &countors](NodePtr node) {
-    std::vector<bool> bimg = node->reconstruct(domain);
-    std::cout << "\nbimage:\n";
-    printImageIntoConsoleWithCast<int>(bimg, domain);
-    std::cout << "\ncontours:\n";
-    std::vector<bool> bcontour = 
-      reconstructContourImage(countors[node->id()], domain);
-    printImageIntoConsoleWithCast<int>(bcontour, domain);
-    std::cout << "\n";
-  }); 
+    tree.traverseByLevel([&domain, &countors](NodePtr node) {
+      std::vector<bool> bimg = node->reconstruct(domain);
+      std::cout << "\nbimage:\n";
+      printImageIntoConsoleWithCast<int>(bimg, domain);
+      std::cout << "\ncontours:\n";
+      std::vector<bool> bcontour = 
+        reconstructContourImage(countors[node->id()], domain);
+      printImageIntoConsoleWithCast<int>(bcontour, domain);
+      std::cout << "\n";
+    });
+  }
+  else {
+    int nx, ny, nc;
+    uint8 *data = stbi_load(argv[1], &nx, &ny, &nc, 0);
 
+    std::vector<uint8> f(data, data + (nx*ny));
+    Box domain = Box::fromSize(UI32Point{nx, ny});
+
+    std::shared_ptr<Adjacency> adj 
+      = std::make_shared<Adjacency4C>(domain);
+    MTree tree = buildMaxTree(f, adj);
+
+    std::vector<std::set<uint32>> countors = 
+      extractCountors(
+        domain, 
+        f, 
+        std::make_shared<InfAdjacency4C>(domain), 
+        tree);
+
+    NodePtr node = tree.smallComponent(domain.pointToIndex(
+      I32Point{nx/2, ny/2}));
+    const std::set<uint32> &contour = countors[node->id()];
+    std::vector<uint8> output(domain.numberOfPoints(), 255);
+    for (uint32 pidx : node->reconstruct())
+      output[pidx] = 0;
+    for (uint32 pidx : contour)
+      output[pidx] = 128;
+
+    stbi_write_png("out.png", nx, ny, 1, output.data(), 0);
+  } 
+  
   return 0;
 }
