03bcf450c7fc723231395a4ea24fc8f2d1754af2
Dennis José da Silva <dennisjosesilva@gmail.com>
2023-04-11 23:03:06 -0300

    make files for conan v2.

--- conanfile.py
+++ conanfile.py
@@ -1,33 +1,49 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class Morphotree(ConanFile):
   name = "morphotree"
   version = "0.0.1"
+
+  # Optinal metadata 
+  author = "dennisjosesilva"
   license = "<Put the package license here>"
   description = "A Morphological tree prototyping library"
-  settings = "os", "compiler", "build_type", "arch"
-  options = {"shared": [True, False], "compiler": ["gcc", "clang"]}
-  default_options = {"shared": False, "compiler": "gcc"}
-  generators = "cmake"
-  #exports_sources = "src/*"
+  url = "https://github.com/dennisjosesilva/morphotree"
+  topics = ("morphological tree", "image processing", "mathematical morphology")
 
-  def export_sources(self):
-    self.copy("*.cpp", dst="src", src="src", excludes="main.cpp")
-    self.copy("CMakeLists.txt", dst="src", src="src", excludes="main.cpp")
-    self.copy("*.hpp", dst="include", src="include")
+  # Binary configuration
+  settings = "os", "compiler", "build_type", "arch"
+  options = {"shared": [True, False], "fPIC": [True, False]}
+  default_options = {"shared": False, "fPIC": True}
+  
+  exports_sources = "src/*.cpp", "src/CMakeLists.txt", "!src/main.cpp", "include/*.hpp"
+
+  def config_options(self):
+    if self.settings.os == "Windows":
+      self.options.rm_safe("fPIC")
+  
+  def configure(self):
+    if self.options.shared:
+      self.options.rm_safe("fPIC")
+
+  def layout(self):
+    cmake_layout(self)
+
+  def generate(self):
+    deps = CMakeDeps(self)
+    deps.generate()
+    tc = CMakeToolchain(self)
+    tc.generate()
 
   def build(self):
     cmake = CMake(self)
-    cmake.configure(source_folder="src")
+    cmake.configure(build_script_folder="src")
     cmake.build()
 
   def package(self):
-    self.copy("*.hpp", dst="include", src="include")
-    self.copy("*.lib", dst="lib", keep_path=False)
-    self.copy("*.dll", dst="bin", keep_path=False)
-    self.copy("*.dylib*", dst="lib", keep_path=False)
-    self.copy("*.so", dst="lib", keep_path=False)
-    self.copy("*.a", dst="lib", keep_path=False)
+    cmake = CMake(self)
+    cmake.install()
 
   def package_info(self):
     self.cpp_info.libs = ["morphotree"]
