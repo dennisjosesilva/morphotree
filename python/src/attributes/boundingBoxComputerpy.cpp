7f986d47ba965688c94ff4ad36bf83f51193b369
Dennis José da Silva <dennisjosesilva@gmail.com>
2023-08-03 14:39:43 -0300

    Removing python library generation code from this repository.

--- python/src/attributes/boundingBoxComputerpy.cpp
+++
@@ -1,9 +0,0 @@
-#include "attributes/boundingBoxComputerpy.hpp"
-
-void bindFoundamentalTypesBoundingBoxComputer(py::module &m)
-{
-  bindBoundingBoxComputer<mt::uint8>(m, "UI8");
-  bindBoundingBoxComputer<mt::int8>(m, "I8");
-  bindBoundingBoxComputer<mt::uint32>(m, "UI32");
-  bindBoundingBoxComputer<mt::int32>(m, "I32");
-}
--- python/src/attributes/volumeComputerpy.cpp
+++
@@ -1,9 +0,0 @@
-#include "attributes/volumeComputerpy.hpp"
-     
-void bindFoundamentalTypesMaxTreeVolumeComputer(py::module &m)
-{
-  bindMaxTreeVolumeComputer<mt::uint8>(m, "UI8");
-  bindMaxTreeVolumeComputer<mt::int8>(m, "I8");
-  bindMaxTreeVolumeComputer<mt::uint32>(m, "UI32");
-  bindMaxTreeVolumeComputer<mt::int32>(m, "I32");
-}
