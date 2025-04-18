cfad878dedbfc8cdec4cfcb8afef1e530c22110e
Dennis Jose da Silva <dennisjosesilva@gmail.com>
2025-04-18 16:41:41 +0200

    test for basic math operators of points.

--- tests/src/core/Test_Point.cpp
+++ tests/src/core/Test_Point.cpp
@@ -24,4 +24,37 @@ TEMPLATE_TEST_CASE("Points can be perform basic math opetarors", "[Point][templa
         r = p.sub(q);
         REQUIRE(r == mt::Point<TestType>{2, 1});
     }
-}
+    SECTION("We can multiply two points") {
+        mt::Point<TestType> r = p * q;
+        REQUIRE(r == mt::Point<TestType>{35, 20});
+
+        r = p.mult(q);
+        REQUIRE(r == mt::Point<TestType>{35, 20});
+    }
+    SECTION("We can divide two points") {
+        mt::Point<TestType> r = p / q;
+        TestType rx = p.x() / q.x();
+        TestType ry = p.y() / q.y();
+
+        REQUIRE(r == mt::Point<TestType>{rx, ry});
+    }
+    SECTION("We can add two points in place") {
+        p += q;
+        REQUIRE(p == mt::Point<TestType>{12, 9});
+    }
+    SECTION("We can subtract two points in place") {
+        p -= q;
+        REQUIRE(p == mt::Point<TestType>{2, 1});
+    }
+    SECTION("We can multiply two points in place") {
+        p *= q;
+        REQUIRE(p == mt::Point<TestType>{35, 20});
+    }
+    SECTION("We can divide two points in place") {
+        TestType rx = p.x() / q.x();
+        TestType ry = p.y() / q.y();
+        p /= q;
+
+        REQUIRE(p == mt::Point<TestType>{rx, ry});
+    }
+ }
